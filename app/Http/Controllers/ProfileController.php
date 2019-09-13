74ef81a15b625dce38f3a391e4613b01762b324d
Irfan Ullah <theirfanullah@gmail.com>
2019-09-13 10:29:47 +0500

    profile image upload functionality coded

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,34 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class ProfileController extends Controller
 {
     //
 
     public function profilePictureUpdate(Request $req){
         $file = $req->file('image');
-    //    $base64_string = $req->input('image');
-    //    $output_file = "./baseimage/img".rand(1,10000).".jpg";
-    //    file_put_contents($output_file, file_get_contents($base64_string));
-    if($req->hasFile('image')){
-        $file->move("./uploads",$file->getClientOriginalName());
-        return response()->json([
-            //  'message' => $_FILES['image']['name'],
-             'message' => $file->getClientOriginalName(),
-            // 'uri' => $_FILES['image']['path'],
-            // //'no' => $_FILES['image']['path'],
-             'rand' => rand(1,1000),
-             'w' => 'moved'
-         ]);
-    }
-        return response()->json([
-           //  'message' => $_FILES['image']['name'],
-            //'message' => $file->getClientOriginalName(),
-        //    'uri' => $_FILES['image']['path'],
-        //    //'no' => $_FILES['image']['path'],
-            'rand' => rand(1,1000),
-            'w' => 'no'
-        ]);
+        $token = $req->input('token');
+
+        $user = User::where(['token' => $token]);
+        if($user->count() > 0){
+            $user = $user->get()->first();
+            if($req->hasFile('image')){
+                $image_name = $file->getClientOriginalName();
+                if($file->move("./uploads/profile/",$image_name)){
+                    $user->profile_image = $image_name;
+                    if($user->save()){
+                        return response()->json([
+                            'isChanged' => true,
+                            'isError' => false,
+                            'message' => 'Profile image changed.'
+                        ]);
+                    }else{
+                        return response()->json([
+                            'isError' => true,
+                            'message' => 'Error occurred in saving the image. Try again.'
+                        ]);
+                    }
+
+                }else {
+                    return response()->json([
+                        'isError' => true,
+                        'message' => 'Error occurred in uploading the image. Try again.'
+                    ]);
+                }
+
+            }else {
+                return response()->json([
+                    'isError' => true,
+                    'message' => 'Image must be provided.'
+                ]);
+            }
+        }else {
+            return response()->json([
+                'isError' => true,
+                'message' => 'Invalid user provided.'
+            ]);
+        }
     }
 }
